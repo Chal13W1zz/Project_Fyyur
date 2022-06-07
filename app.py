44e3caf8df8357c8d083936cd749534963861fba
Chal13W1zz <charles.muthii@student.moringaschool.com>
2022-06-07 16:59:52 +0300

    change port to 8080

--- app.py
+++ app.py
@@ -634,7 +634,6 @@ if not app.debug:
 # Or specify port manually:
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    port = int(os.environ.get('PORT', 3000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
